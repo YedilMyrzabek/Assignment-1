44aa14b407d75650cb268de594cdd5d77ec603ba
yedil <230760@astanait.edu.kz>
2023-12-26 10:34:12 +0600

    Create Point's Constructor and x,y

--- src/Point.java
+++ src/Point.java
@@ -1,2 +1,10 @@
 public class Point {
+    private double x;
+    private double y;
+
+    //constructor having both parameters
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
 }
