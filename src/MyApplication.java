7f9cf77b786730cc4ac4c0b1ecc5c6f6ef236edd
yedil <230760@astanait.edu.kz>
2023-12-26 10:40:34 +0600

    Create File and Connect with Java

--- src/MyApplication.java
+++ src/MyApplication.java
@@ -1,3 +1,21 @@
-public class MyApplication {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
+public class MyApplication {
+    public static void main(String... args) throws FileNotFoundException {
+        //Create file and connect
+        File file = new File("C:\\\\Doc\\\\java.txt");
+
+        Scanner sc = new Scanner(file);
+        Shape shape = new Shape();
+
+        //read this file
+        while (sc.hasNext()) {
+            double x = sc.nextDouble();
+            double y = sc.nextDouble();
+
+            Point point = new Point(x, y);
+        }
+    }
 }
